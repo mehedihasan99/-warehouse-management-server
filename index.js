853de9406ca03cbcecd78d32bab8928f1562490b
mehedihasan99 <hmhasan.mehedi2003@gmail.com>
2022-05-06 14:41:30 +0600

    node_modules be gone!

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send("No tension, It works");
+})
+app.listen(port, () => {
+    console.log("Listening to port", port);
+})

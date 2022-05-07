65aec848854bf07dbf499c0c6e02d9c6dbaf7d0f
mehedihasan99 <hmhasan.mehedi2003@gmail.com>
2022-05-07 10:22:29 +0600

    filter email

--- index.js
+++ index.js
@@ -23,6 +23,14 @@ async function run() {
             const clothes = await cursor.toArray();
             res.send(clothes)
         })
+        // 
+        app.get('/clothes', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const cursor = clothCollection.find(query);
+            const clothes = await cursor.toArray();
+            res.send(clothes)
+        })
         // get specific one item from database( by id)
         app.get('/cloth/:id', async (req, res) => {
             const id = req.params.id;
@@ -30,7 +38,19 @@ async function run() {
             const cloth = await clothCollection.findOne(query);
             res.send(cloth);
         })
-
+        // post :received data from the client and send to database
+        app.post('/cloth', async (req, res) => {
+            const newCloth = req.body;
+            const result = await clothCollection.insertOne(newCloth);
+            res.send(result);
+        })
+        // delete: 
+        app.delete('/cloth/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await clothCollection.deleteOne(query);
+            res.send(result);
+        })
     }
     finally {
 
